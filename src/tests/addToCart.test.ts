6aff2f0c2fc7941b8c0deb9d96b95defd2649f86
Ashokkumar GM <gmashok.mohan@gmail.com>
2025-09-24 18:54:46 +0200

    feat(e2e): update tests with fixtures

--- src/tests/addToCart.test.ts
+++ src/tests/addToCart.test.ts
@@ -1,23 +1,12 @@
-import {test, expect} from '@playwright/test';
-import {LoginPage} from '../pages/LoginPage';
-import {InventoryPage} from '../pages/InventoryPage';
-import {loadTestData} from '../utils/testUtils';
+import {test, expect} from '@fixtures';
 
 test.describe('Sauce Demo - Add to Cart', () => {
-    let testData: any;
-
-    test.beforeAll(async () => {
-        testData = await loadTestData();
-    });
-
-    test.beforeEach(async ({page}) => {
-        const loginPage = LoginPage(page);
+    test.beforeEach(async ({loginPage, testData}) => {
         await loginPage.navigate();
         await loginPage.login(testData.users.standard.username, testData.users.standard.password);
     });
 
-    test('Add items to cart', async ({page}) => {
-        const inventoryPage = InventoryPage(page);
+    test('Add items to cart', async ({inventoryPage, page}) => {
         await inventoryPage.addItemToCart(0);
         await inventoryPage.goToCart();
         const cartItemCount = await page.$$eval('.cart_item', (items) => items.length);
